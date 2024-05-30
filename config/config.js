ea2fc2e061f4044e1bcc10fec182b9e3ed3a0e3d
Richard Essuman <ressuman001@gmail.com>
2024-05-30 21:38:32 +0000

    chore: Add .env and node_modules to .gitignore

---
+++ config/config.js
@@ -0,0 +1,5 @@
+export const APPLICATION_ID =
+  process.env.APPLICATION_ID || "kd7I_wO10OrPBDApTa1tuaiVHo7smVoaw0ptJC1JTKI";
+export const SECRET =
+  process.env.SECRET || "Aok4cWAnrjIyPLyo87pvBpyfSPTI3bk4QYC6gIfJPCM";
+export const CALLBACK_URL = process.env.CALLBACK_URL || "http://localhost:5173";
