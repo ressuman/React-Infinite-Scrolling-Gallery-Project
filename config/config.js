52e0bc929de097e08bb123cc6dc67b1473487d7d
Richard Essuman <ressuman001@gmail.com>
2024-05-30 21:38:12 +0000

    removed dependencies

--- config/config.js
+++
@@ -1,5 +0,0 @@
-export const APPLICATION_ID =
-  process.env.APPLICATION_ID || "kd7I_wO10OrPBDApTa1tuaiVHo7smVoaw0ptJC1JTKI";
-export const SECRET =
-  process.env.SECRET || "Aok4cWAnrjIyPLyo87pvBpyfSPTI3bk4QYC6gIfJPCM";
-export const CALLBACK_URL = process.env.CALLBACK_URL || "http://localhost:5173";
